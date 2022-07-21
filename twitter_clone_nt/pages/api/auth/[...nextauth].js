9c90a2dc5650b78e3cf0705ba6c4f140ba20173a
hotan48666 <hotan48666@gmail.com>
2022-07-21 22:57:00 +0900

    [...nextauth] add

--- twitter_clone_nt/pages/api/auth/[...nextauth].js
+++ twitter_clone_nt/pages/api/auth/[...nextauth].js
@@ -16,6 +16,7 @@ export default NextAuth({
             
             session.user.uid = token.sub;
             return session;
-        }
-    }
+        },
+    },
+    secret:process.env.JWT_SECRET
 })
